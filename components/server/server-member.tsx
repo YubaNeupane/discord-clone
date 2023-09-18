f382bb008453b85cae511cfb77f5b4da96a95129
YubaNeupane <neopaneyb2@gmail.com>
2023-09-18 15:23:12 -0400

    fixing conversation naviagation bug and adding converation

--- components/server/server-member.tsx
+++ components/server/server-member.tsx
@@ -26,7 +26,7 @@ export const ServerMember = ({ member, server }: ServerMemberProps) => {
   const icon = roleIconMap[member.role];
 
   const onClick = () => {
-    router.push(`/server/${params?.serverId}/conversations/${member.id}}`);
+    router.push(`/server/${params?.serverId}/conversations/${member.id}`);
   };
 
   return (
